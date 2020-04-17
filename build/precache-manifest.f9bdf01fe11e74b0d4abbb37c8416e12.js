fcc4a6571e4d9a71088018c74e0905360e42dda8
Aditya Tippanaboena <aditya@Adityas-MacBook-Pro.local>
2020-04-17 07:12:36 +0530

    Build Added

---
+++ build/precache-manifest.f9bdf01fe11e74b0d4abbb37c8416e12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb440fb1503dc74193ab151beb6086fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f889e26328029516d8f",
+    "url": "/static/css/2.03c19fbc.chunk.css"
+  },
+  {
+    "revision": "a207623129b31ebd3e45",
+    "url": "/static/css/main.7189534f.chunk.css"
+  },
+  {
+    "revision": "7f889e26328029516d8f",
+    "url": "/static/js/2.03bfae58.chunk.js"
+  },
+  {
+    "revision": "f1754b8003962f98267404606781bc1a",
+    "url": "/static/js/2.03bfae58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a207623129b31ebd3e45",
+    "url": "/static/js/main.5dc8d896.chunk.js"
+  },
+  {
+    "revision": "0d1179c663ff223ba99b",
+    "url": "/static/js/runtime-main.c28d318d.js"
+  }
+]);
